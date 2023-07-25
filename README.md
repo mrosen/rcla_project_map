016a08819ba5c9f661e1a8f6a690dd65e797a4f7
Michael Rosen <michael.rosen@gmail.com>
2023-07-24 20:35:04 -0600

    add status to POI, colored markers.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # rcla_project_map
 map of projects for Rotary Club of Lake Atitlan
+
+
+Currently uses my personal Google Maps API key.
+
+Here's where I can see the billing
+  https://console.cloud.google.com/billing/0166F7-B38A06-FF1322?project=elite-hangar-388623
+
+Here's where I keep all this.
+  https://github.com/mrosen/rcla_project_map
+
+It uses GitHub Pages to host the site:
+  https://mrosen.github.io/rcla_project_map/index.html
+
+
+To test locally, 
+  python3 -m http.server
+  http://localhost:8000/
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function initMap() {
+    console.log('initMap().');
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
         center: { lat: 14.703454, lng: -91.191623 },
@@ -6,14 +7,14 @@ function initMap() {
 
     const pointsOfInterest = [
         // Add your points of interest here
-        { title: 'GG2344748', position: { lat: 14.765853, lng: -91.179493 }, url: 'https://example.com/point1', description: 'Vista Hermosa Water & Sanitation (Draft)' },
-        { title: 'GG2352598', position: { lat: 14.7652, lng: -91.13217 }, url: 'https://example.com/point2', description: '*Patantic* & Sanika-ya (Submitted)' },
-        { title: 'GG2352598', position: { lat: 14.63454, lng: -91.15257 }, url: 'https://example.com/point2', description: 'Patantic & *Sanika-ya* (Submitted)' },
-        { title: 'GG2346063', position: { lat: 14.74219, lng: -91.2044 }, url: 'https://example.com/point2', description: 'Amigos de Santa Cruz (Approved)' },
-        { title: 'GG2091778', position: { lat: 14.7708, lng: -91.26555 }, url: 'https://example.com/point2', description: 'Water & Sanitation in *Santa Lucia Utatlan* and San Andreas (Approved)' },
-        { title: 'GG2091778', position: { lat: 14.74625, lng: -91.13336 }, url: 'https://example.com/point2', description: 'Water & Sanitation in Santa Lucia Utatlan and *San Andreas* (Approved)' },
-        { title: 'GG2125241', position: { lat: 14.72823, lng: -91.07540 }, url: 'https://example.com/point2', description: 'WASH El Potrero (Approved)' },
-        { title: 'GG1873788', position: { lat: 14.63551, lng: -91.14264 }, url: 'https://example.com/point2', description: 'Stoves in San Lucas (Approved)' },
+        { title: 'GG2344748', position: { lat: 14.765853, lng: -91.179493 }, url: 'https://example.com/point1', description: 'Vista Hermosa Water & Sanitation (Draft)' , status: 'draft'},
+        { title: 'GG2352598', position: { lat: 14.7652, lng: -91.13217 }, url: 'https://example.com/point2', description: '*Patantic* & Sanika-ya (Submitted)' , status: 'submitted'},
+        { title: 'GG2352598', position: { lat: 14.63454, lng: -91.15257 }, url: 'https://example.com/point2', description: 'Patantic & *Sanika-ya* (Submitted)', status: 'submitted' },
+        { title: 'GG2346063', position: { lat: 14.74219, lng: -91.2044 }, url: 'https://example.com/point2', description: 'Amigos de Santa Cruz (Approved)', status: 'approved' },
+        { title: 'GG2091778', position: { lat: 14.7708, lng: -91.26555 }, url: 'https://example.com/point2', description: 'Water & Sanitation in *Santa Lucia Utatlan* and San Andreas (Approved)', status: 'approved' },
+        { title: 'GG2091778', position: { lat: 14.74625, lng: -91.13336 }, url: 'https://example.com/point2', description: 'Water & Sanitation in Santa Lucia Utatlan and *San Andreas* (Approved)', status: 'approved' },
+        { title: 'GG2125241', position: { lat: 14.72823, lng: -91.07540 }, url: 'https://example.com/point2', description: 'WASH El Potrero (Approved)', status: 'approved/delinquent' },
+        { title: 'GG1873788', position: { lat: 14.63551, lng: -91.14264 }, url: 'https://example.com/point2', description: 'Stoves in San Lucas (Approved)', status: 'approved/delinquent' },
     ];
 
     pointsOfInterest.forEach(point => {
@@ -30,10 +31,32 @@ function initMap() {
             content: infoWindowContent
         });
 
+
+        // Choose a different marker color based on the status
+        let markerColor;
+        switch (point.status) {
+            case 'draft':
+                markerColor = 'purple';
+                break;
+            case 'submitted':
+                markerColor = 'blue';
+                break;
+            case 'approved':
+                markerColor = 'green';
+                break;
+            case 'approved/delinquent':
+                markerColor = 'yellow';
+                break;
+            default:
+                markerColor = 'red';  // Default color, used if the status doesn't match any of the above
+        }
+        console.log(point.status, markerColor);
+
         const marker = new google.maps.Marker({
             position: point.position,
             map: map,
-            title: point.title
+            title: point.title,
+            icon: `http://maps.google.com/mapfiles/ms/icons/${markerColor}-dot.png`  // Use the chosen color for the marker
         });
 
         marker.addListener('click', function() {
